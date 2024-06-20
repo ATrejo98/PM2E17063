defbdf42256e318eef665b2bc440b8f853997594
ATrejo <anghelardon2015@gmail.com>
2024-06-20 09:00:46 -0600

    Actualizacion del repo.

--- Views/PageHome.xaml.cs
+++ Views/PageHome.xaml.cs
@@ -1,9 +1,27 @@
-namespace PM2E17063.Views;
+using Microsoft.Maui.Controls;
 
-public partial class PageHome : ContentPage
+namespace PM2E17063.Views
 {
-	public PageHome()
-	{
-		InitializeComponent();
-	}
+    public partial class PageHome : ContentPage
+    {
+        public PageHome()
+        {
+            InitializeComponent();
+        }
+
+        private async void OnAgregarClicked(object sender, EventArgs e)
+        {
+            
+        }
+
+        private async void OnListaClicked(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void OnSalirClicked(object sender, EventArgs e)
+        {
+            
+        }
+    }
 }
