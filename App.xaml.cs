fae68585fca890369e6754b1edfdabbbbe6cbf4a
yefrinp <benigno.amaya1994@gmail.com>
2024-06-20 16:07:39 -0600

    Pantalla Agregar, falta Lat Long Automatico

--- App.xaml.cs
+++ App.xaml.cs
@@ -2,11 +2,31 @@
 {
     public partial class App : Application
     {
+
+        static Controllers.DBSitios dBSitios;
+        public static Controllers.DBSitios Instancia
+        {
+            get
+            {
+                if (dBSitios == null)
+                {
+                    String dbname = "SitiosDB.db3";
+                    String dbpath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+                    String dbfulp = Path.Combine(dbpath, dbname);
+                    dBSitios = new Controllers.DBSitios(dbfulp);
+
+                }
+                return dBSitios;
+            }
+        }
+
+
         public App()
         {
             InitializeComponent();
 
             MainPage = new NavigationPage(new Views.PageHome());
         }
+
     }
 }
--- Views/PageHome.xaml.cs
+++ Views/PageHome.xaml.cs
@@ -1,17 +1,133 @@
-using Microsoft.Maui.Controls;
+using PM2E17063.Models;
+
 
 namespace PM2E17063.Views
 {
     public partial class PageHome : ContentPage
     {
+
+        FileResult photo;
         public PageHome()
         {
             InitializeComponent();
+            obtenerLatitudLongitud();
         }
+        //Obtener GPS
+        public async void obtenerLatitudLongitud()
+        {
+            try
+            {
+                var georequest = new GeolocationRequest(GeolocationAccuracy.Best, TimeSpan.FromSeconds(10));
+
+                var tokendecancelacion = new System.Threading.CancellationTokenSource();
+
+                var localizacion = await Geolocation.GetLocationAsync(georequest, tokendecancelacion.Token);
+
+
+                if (localizacion != null)
+                {
+                    txtlatitud.Text = localizacion.Latitude.ToString();
+                    txtlongitud.Text = localizacion.Longitude.ToString();
+                }
+            }
+            catch (FeatureNotSupportedException fnsEx)
+            {
+                await DisplayAlert("Advertencia", "Este dispositivo no soporta GPS" + fnsEx, "Ok");
+            }
+            catch (FeatureNotEnabledException)
+            {
+                await DisplayAlert("Advertencia", "Su GPS se encuentra desactivado, favor volver a ingresar con el GPS activado", "Ok");
+                System.Diagnostics.Process.GetCurrentProcess().Kill();
+
+            }
+            catch (PermissionException pEx)
+            {
+                await DisplayAlert("Advertencia", "Sin Permisos de Geolocalizacion" + pEx, "Ok");
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Advertencia", "Sin Ubicacion " + ex, "Ok");
+            }
+        }
+
+
+        byte[] imageToSave;
+        //void para tomar la foto
+        private async void btnfoto_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                var photo = await MediaPicker.Default.CapturePhotoAsync();
+
+                if (photo != null)
+                {
+                    imageToSave = null;
+
+                    string localizacion = Path.Combine(FileSystem.CacheDirectory, photo.FileName);
+
+                    using (Stream sourceStream = await photo.OpenReadAsync())
+                    {
+                        using (FileStream imagenLocal = File.OpenWrite(localizacion))
+                        {
+                            await sourceStream.CopyToAsync(imagenLocal);
+                        }
+
+                        sourceStream.Position = 0;
+
+                        using (MemoryStream memoryStream = new MemoryStream())
+                        {
+                            await sourceStream.CopyToAsync(memoryStream);
+                            imageToSave = memoryStream.ToArray();
+                        }
+
+                        img.Source = ImageSource.FromStream(() => new MemoryStream(imageToSave));
+                    }
+
+                    obtenerLatitudLongitud();
+                    txtdescripcion.Focus();
+                }
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Error", "Se ha generado el siguiente error al agregar la imagen: " + ex.Message, "Aceptar");
+            }
+        }
+
+
+
 
         private async void OnAgregarClicked(object sender, EventArgs e)
         {
-            
+            if (imageToSave == null)
+            {
+                await DisplayAlert("Aviso!", "Ingrese una imagen del sitio", "Aceptar");
+            }
+            else if (txtdescripcion.Text == null)
+            {
+                await DisplayAlert("Aviso!", "Ingrese una descripcion del sitio", "Aceptar");
+            }
+            else
+            {
+                var sitio = new Sitios { imagen = imageToSave, longitud = txtlatitud.Text, latitud = txtlongitud.Text, descripcion = txtdescripcion.Text };
+                var resultado = await App.Instancia.sitioSave(sitio);
+
+                if (resultado != 0)
+                {
+                    await DisplayAlert("Aviso", "Sitio registrado con exito!", "Aceptar");
+                    txtdescripcion.Text = "";
+                    img.Source = "anadir.png";
+                    imageToSave = null;
+
+                }
+                else
+                {
+                    await DisplayAlert("Aviso", "Ha Ocurrido un Error", "Aceptar");
+                }
+
+
+                await Navigation.PopAsync();
+            }
+
         }
 
         private async void OnListaClicked(object sender, EventArgs e)
@@ -21,7 +137,9 @@ namespace PM2E17063.Views
 
         private void OnSalirClicked(object sender, EventArgs e)
         {
-            
+            // Proceso para salir
+            System.Diagnostics.Process.GetCurrentProcess().Kill();
+
         }
     }
 }
