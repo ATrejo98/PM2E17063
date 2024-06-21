5cc6de74a8c590311ff13c6cc1e78dc6d63b5a19
ATrejo <anghelardon2015@gmail.com>
2024-06-20 22:55:11 -0600

    Implementacion de eliminacion de objetos de la lista mediante checkbox, y compartir imagenes de las selecciones realizadas.

--- Models/sitios.cs
+++ Models/sitios.cs
@@ -1,17 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Text;
 using SQLite;
 
 namespace PM2E17063.Models
 {
-    public class Sitios
+    public class Sitios : INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler PropertyChanged;
+
         [PrimaryKey, AutoIncrement]
-        public int  id { get; set; }    
-        public string longitud { get; set; }  
+        public int id { get; set; }
+        public string longitud { get; set; }
         public string latitud { get; set; }
         public string descripcion { get; set; }
-        public byte[] imagen { get; set; }    
+        public byte[] imagen { get; set; }
+
+        private bool _isSelected;
+        public bool IsSelected
+        {
+            get { return _isSelected; }
+            set
+            {
+                if (_isSelected != value)
+                {
+                    _isSelected = value;
+                    OnPropertyChanged(nameof(IsSelected));
+                }
+            }
+        }
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
--- Views/PageList.xaml.cs
+++ Views/PageList.xaml.cs
@@ -1,8 +1,7 @@
-using System;
 using System.Collections.ObjectModel;
+using System.Windows.Input;
 using PM2E17063.Models;
 using PM2E17063.Controllers;
-using System.Threading.Tasks;
 
 namespace PM2E17063.Views
 {
@@ -11,13 +10,21 @@ namespace PM2E17063.Views
         public ObservableCollection<Sitios> SitiosCollection { get; set; }
         private readonly DBSitios _dbSitios;
 
+        public ICommand DeleteSelectedCommand { get; }
+        public ICommand ShareImageSelectedCommand { get; }
+
         public PageList()
         {
             InitializeComponent();
 
-            _dbSitios = App.Instancia; 
+            _dbSitios = App.Instancia;
+
+            DeleteSelectedCommand = new Command(async () => await OnDeleteCommand(), CanExecuteDeleteOrShare);
+            ShareImageSelectedCommand = new Command(async () => await OnShareCommandImage(), CanExecuteDeleteOrShare);
 
             LoadData();
+
+            BindingContext = this;
         }
 
         private async void LoadData()
@@ -25,8 +32,20 @@ namespace PM2E17063.Views
             try
             {
                 var sitiosList = await _dbSitios.ObtenerlistadoSitio();
-
                 SitiosCollection = new ObservableCollection<Sitios>(sitiosList);
+
+                // Monitorea cambios en la selección para habilitar/deshabilitar botones
+                foreach (var sitio in SitiosCollection)
+                {
+                    sitio.PropertyChanged += (s, e) =>
+                    {
+                        if (e.PropertyName == nameof(Sitios.IsSelected))
+                        {
+                            (DeleteSelectedCommand as Command)?.ChangeCanExecute();
+                            (ShareImageSelectedCommand as Command)?.ChangeCanExecute();
+                        }
+                    };
+                }
 
                 sitiosCollectionView.ItemsSource = SitiosCollection;
             }
@@ -35,5 +54,65 @@ namespace PM2E17063.Views
                 await DisplayAlert("Error", "Error al cargar datos: " + ex.Message, "Aceptar");
             }
         }
+
+
+
+
+        private bool CanExecuteDeleteOrShare()
+        {
+            return SitiosCollection != null && SitiosCollection.Any(s => s.IsSelected);
+        }
+
+        private async Task OnDeleteCommand()
+        {
+            var selectedItems = SitiosCollection.Where(s => s.IsSelected).ToList();
+
+            if (selectedItems.Any())
+            {
+                bool confirm = await Application.Current.MainPage.DisplayAlert("Confirmación", "¿Estás seguro de querer eliminar los elementos seleccionados?", "Sí", "Cancelar");
+
+                if (confirm)
+                {
+                    foreach (var sitio in selectedItems)
+                    {
+                        await _dbSitios.eliminarsitio(sitio);
+                        SitiosCollection.Remove(sitio);
+                    }
+                }
+            }
+            else
+            {
+                await Application.Current.MainPage.DisplayAlert("Alerta", "No hay elementos seleccionados para eliminar.", "Aceptar");
+            }
+        }
+
+        private async Task OnShareCommandImage()
+        {
+            var selectedItems = SitiosCollection.Where(s => s.IsSelected).ToList();
+
+            if (selectedItems.Count == 0)
+            {
+                await Application.Current.MainPage.DisplayAlert("Alerta", "No hay elementos seleccionados para compartir.", "Aceptar");
+                return;
+            }
+
+            List<ShareFile> shareFiles = new List<ShareFile>();
+
+            // Recorrer los elementos seleccionados y guardar cada imagen en almacenamiento temporal
+            foreach (var sitio in selectedItems)
+            {
+                var tempImagePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), $"{sitio.id}_sharedimage.png");
+                File.WriteAllBytes(tempImagePath, sitio.imagen);
+                shareFiles.Add(new ShareFile(tempImagePath));
+            }
+
+            // Compartir las imágenes usando .NET MAUI
+            await Share.RequestAsync(new ShareMultipleFilesRequest
+            {
+                Title = "Compartir imágenes",
+                Files = shareFiles
+            });
+        }
+
     }
 }
